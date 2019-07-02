d789a1c9ef680c87e1fbd11766800b7f985163bb
wuxiaomeng94 <806620602@qq.com>
2019-07-02 10:44:47 +0800

    数据库配置

--- src/test/java/com/capinfo/dcxm/dmonitor/DmonitorApplicationTests.java
+++ src/test/java/com/capinfo/dcxm/dmonitor/DmonitorApplicationTests.java
@@ -2,6 +2,7 @@ package com.capinfo.dcxm.dmonitor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DmonitorApplicationTests {
 
+    /*@Autowired
+    private CapYesswCaseDao capYesswCaseDao;
+
+    @Autowired
+    private A15Dao a15Dao;*/
+
+
     @Test
     public void contextLoads() {
+
+        /*List<CapYesswCase> byYesswNumber = capYesswCaseDao.findByYesswNumber("[电]20194353453");
+        for (CapYesswCase capYesswCase : byYesswNumber) {
+            System.out.println(capYesswCase.getYesswNumber());
+        }*/
+        /*CapYesswCase capYesswCase = new CapYesswCase();
+        capYesswCase.setId(UUID.randomUUID().toString().replaceAll("-",""));
+        capYesswCase.setYesswNumber("[电]20190628");
+        capYesswCaseDao.save(capYesswCase);*/
+
+
+        //A15 a15 = a15Dao.getOne("01db87e1247aeb8f2f714e700446d60fc");
+        //System.out.println(a15.getId());
+
     }
+
+
+
+
 
 }
